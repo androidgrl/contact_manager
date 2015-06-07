fd5cef90800b91301c6149ab74eca9f02e7859b1
androidgrl <kawaharajamie@gmail.com>
2015-06-07 13:31:54 -0600

    Add Company scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150607141610) do
+ActiveRecord::Schema.define(version: 20150607193121) do
+
+  create_table "companies", force: :cascade do |t|
+    t.text     "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "email_addresses", force: :cascade do |t|
     t.text     "address"
